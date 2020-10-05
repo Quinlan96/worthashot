6ef3e53572e502fd71b9090be90ac1ab92ecc304
Robert Quinlan <quinlan96@live.com.au>
2020-10-05 16:11:17 +1000

    Script location changed in ecosystem file

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 		{
 			name: "worthashot",
-			script: "./dist/bin/www.js",
+			script: "./build/bin/www.js",
 			node_args: "-r dotenv/config",
 			env: {
 				"NODE_ENV": "development",
